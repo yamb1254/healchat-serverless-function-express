a7c33e7fe16bf98adc184e7d11b02507706c9f7c
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-16 00:22:06 +0300

    1

--- api/utils/jwtUtils.ts
+++
@@ -1,6 +0,0 @@
-import jwt from "jsonwebtoken";
-import { config } from "../config/envConfig";
-
-export const generateToken = (userId: number) => {
-  return jwt.sign({ userId }, config.jwtSecret, { expiresIn: "1h" });
-};
