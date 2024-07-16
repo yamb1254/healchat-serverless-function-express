901d9c403330243922e0aee3d1416dbd537e2cab
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-16 16:44:23 +0300

    update

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import authRoutes from "./routes/authRoutes"; // Ensure this path is correct
-import chatRoutes from "./routes/chatRoutes";
+import authRoutes from "./src/routes/authRoutes"; // Ensure this path is correct
+import chatRoutes from "./src/routes/chatRoutes";
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -12,6 +12,9 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
+
+app.get('/',(req,res)=>res.send('hello world!'));
+
 app.use(bodyParser.json());
 
 app.use("/api/auth", authRoutes);
