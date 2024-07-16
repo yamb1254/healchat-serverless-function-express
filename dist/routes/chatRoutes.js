0a9f2055e747152dc3852aa97d9404d5968eccc3
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-16 18:37:45 +0300

    113

--- dist/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const chatController_1 = require("../controllers/chatController");
-const authMiddleware_1 = require("../middlewares/authMiddleware");
-const router = (0, express_1.Router)();
-router.post("/send", authMiddleware_1.verifyToken, chatController_1.uploadMiddleware, chatController_1.sendMessage);
-router.get("/messages", authMiddleware_1.verifyToken, chatController_1.getMessages);
-exports.default = router;
