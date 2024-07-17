00056447e5a9799dade1713cb0bea4630961b8b0
Yam Biton <74289122+yamb1254@users.noreply.github.com>
2024-07-17 07:58:00 +0300

    21451

--- index.ts
+++ index.ts
@@ -1,24 +1,20 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
-import authRoutes from "./src/routes/authRoutes"; 
+import authRoutes from "./src/routes/authRoutes";
 import chatRoutes from "./src/routes/chatRoutes";
 
 const app = express();
-const port = process.env.PORT || 80;
-
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 80;
 
 app.use(cors());
-
 app.use(bodyParser.json());
 
-app.get('/',(req,res)=>res.send('hello world!'));
-
+app.get('/', (req: Request, res: Response) => res.send('Hello world!'));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
-  // console.log("Environment configuration:", process.env);
 });
--- public/index.js
+++ public/index.js
@@ -9,13 +9,12 @@ const cors_1 = __importDefault(require("cors"));
 const authRoutes_1 = __importDefault(require("./src/routes/authRoutes"));
 const chatRoutes_1 = __importDefault(require("./src/routes/chatRoutes"));
 const app = (0, express_1.default)();
-const port = process.env.PORT || 80;
+const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 80;
 app.use((0, cors_1.default)());
 app.use(body_parser_1.default.json());
-app.get('/', (req, res) => res.send('hello world!'));
+app.get('/', (req, res) => res.send('Hello world!'));
 app.use("/api/auth", authRoutes_1.default);
 app.use("/api/chat", chatRoutes_1.default);
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-    // console.log("Environment configuration:", process.env);
 });
